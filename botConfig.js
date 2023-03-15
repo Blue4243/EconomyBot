594f76912c86412f4be5e68d50899316a2bc6b12
Blue4243 <127968548+Blue4243@users.noreply.github.com>
2023-03-15 11:35:01 -0400

    Update botConfig.js

--- botConfig.js
+++ botConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
-    token: "YOUR_TOKEN",
-    prefix: "PREFIX",
+    token: "MTA4NTU4NDgzNDI3NTA1NzY5NA.GlPFBI.__Ak-Fd0Ti8SKv3U1oxXCcXLOZh6f700JRcvQk",
+    prefix: "z.",
     admins: [
-        "PEOPLE WHO CAN USE ADD MONEY (IDS)"
+        "1085579946279325817"
 ],
     debug: true,
     countChannel: "countChannelID"
